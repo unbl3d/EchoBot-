ad7fc715b65ef25d2de19be6ba14c032989e11f0
unbl3d <yazanossman424@gmail.com>
2026-06-13 14:19:26 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        // Text users will see (example: "Playing /help | Echo Bot").
+        name: "I LOVE FLYINGCAT",
         // Activity type number (0 = Playing).
         type: 0, 
       },
